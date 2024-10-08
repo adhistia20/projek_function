c4bbd501c6d61904de27922f891fa408df92c866
chayyarra <chayya.igda.897@gmail.com>
2024-10-08 01:15:49 -0700

    Update Fahrenheit

--- programkonversisuhu.cpp
+++ programkonversisuhu.cpp
@@ -15,14 +15,20 @@ int main (){
 
     cout << "Pilih konversi suhu:\n";
     cout << "1. Celcius ke Fahrenheit (C)\n";
+    cout << "2. Fahrenheit ke Celcius (F)\n";
+    cout << "Masukkan pilihan (C/F): ";
     cin >> pilihan;
 
     if (pilihan == 'C' || pilihan == 'c') {
         cout << "Masukkan suhu dalam Celcius: ";
         cin >> suhu;
         cout << suhu << "°C = " << celciusToFahrenheit(suhu) << "°F\n";
+    }esle if (pilihan == 'F' || pilihan == 'f') {
+        cout << "Masukkan suhu dalam Fahrenheit: ";
+        cin >> suhu;
+        cout << suhu << "°C = " << fahrenheitToCelcius(suhu) << "°C\n";
+    }else{
+        cout << "Pilihan tidak valid.\n";
     }
-
-
     return 0;
 }
